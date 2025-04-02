7048b3b9de9d2bd81cbe42b79fae5080fc30de17
Manish Kumar <123621589+its-manishks@users.noreply.github.com>
2025-04-02 18:25:41 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,26 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+  const mongoURI = process.env.MONGO_URI || 'mongodb://localhost/sentify';
+  let retries = 5;
+  while (retries) {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost/sentify', {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+      const conn = await mongoose.connect(mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+      console.log(`MongoDB Connected: ${conn.connection.host}`);
+      return; // Successful connection, exit the function.
     } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
+      console.error(`Error connecting to MongoDB: ${error.message}`);
+      retries -= 1;
+      console.log(`Retries left: ${retries}`);
+      // Wait 5 seconds before retrying.
+      await new Promise(res => setTimeout(res, 5000));
     }
+  }
+  console.error('Failed to connect to MongoDB after several attempts. Exiting...');
+  process.exit(1);
 };
 
 module.exports = connectDB;
